1c579a05cbeb8c0aa5297862e2b16238416c5979
Murthy Yanamandra <murthy.viny@gmail.com>
2021-10-20 09:48:55 +0530

    Added some comments and updated README.md file

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 Steps to run the code:
     1) Install Requirements.txt
     2) Run main.py file
+
+Files:
+
+    Locators:
+        This folder contains all the objects present in a particular page
+    
+    Pages:
+        This folder contains all the functionalities or the actions we can perform in a particular page.
+        But, for this task purpose, I have created functions for only required actions
+    
+    Utils:
+        This folder has got some utilities that we might require in our code.
+        In this folder, I have placed all the constants we may use in our code.
+        
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from selenium.webdriver import Chrome
 from settings import CHROME_DRIVER_PATH
-from pages.canvaspage import CanvasPage
-from utils.consts import Constants
+from pages.canvaspage import CanvasPage  # This is the class which had all the actions we can perform in the page
+from utils.consts import Constants  # This is the class which has all the constants
 import time
 
 
 def perform_task():
+    """This method performs all the drawing and erasing tasks, Takes no args"""
     cp.draw_line(Constants.HORIZONTAL_LINE_START, Constants.HORIZONTAL_LINE_END)
     cp.draw_line(Constants.VERTICAL_LINE_START, Constants.VERTICAL_LINE_END)
     cp.draw_rectangle(Constants.RECTANGLE_START, Constants.RECTANGLE_END)
@@ -18,11 +19,11 @@ if __name__ == '__main__':
     driver = Chrome(CHROME_DRIVER_PATH)
     # driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
     driver.get(Constants.URL)
-    driver.fullscreen_window()
+    driver.fullscreen_window()  # Maximising the window
     cp = CanvasPage(driver)
     perform_task()
     time.sleep(3)
-    driver.quit()
+    driver.quit()  # Quitting the browser
 
 
 
